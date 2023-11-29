da76df9fa841a7555e9183bdd88d0ba900095d8d
ichiro-ss <sei.swmr@gmail.com>
2023-11-29 18:44:17 +0900

    solved st4

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 // DO NOT DELETE
 
+import { useState } from 'react'
 import './App.css'
 
 /**
  * @type {() => JSX.Element}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] = useState(
+    'https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg',
+  )
   return (
     <body>
       <header>Dog App</header>
       <h1>犬の画像を表示するサイトです!</h1>
-      <img src="https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg" />
+      <img src={dogUrl} />
     </body>
   )
 }
