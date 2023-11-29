92293a8704deb6db068e9f9fc7e4620b82070e51
ichiro-ss <sei.swmr@gmail.com>
2023-11-29 18:31:28 +0900

    solved st3

--- src/App.jsx
+++ src/App.jsx
@@ -6,5 +6,11 @@ import './App.css'
  * @type {() => JSX.Element}
  */
 export const App = () => {
-  return <header>App</header>
+  return (
+    <body>
+      <header>Dog App</header>
+      <h1>犬の画像を表示するサイトです!</h1>
+      <img src="https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg" />
+    </body>
+  )
 }
